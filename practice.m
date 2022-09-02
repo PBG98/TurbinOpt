76941916cec77b0127484e2f37921c02b2aded86
ByeongGuk Park <ppkj58220@naver.com>
2022-09-02 15:30:04 +0900

    220831

--- practice.m
+++ practice.m
@@ -1,2 +1,5 @@
-rng default
-X = lhsdesign(10,4)
+lhs_B_tip = lhsdesign_modified(10, 0.901,1.306);
+lhs_B_70 = lhsdesign_modified(10, 00.913,1.198);
+lhs_A_tip = lhsdesign_modified(10, 0.497,1.553);
+lhs_A_70 = lhsdesign_modified(10, 0.489,1.466);
+lhs_turbin_param = horzcat(lhs_B_tip, lhs_B_70, lhs_A_tip, lhs_A_70)
--- rbnn.m
+++ rbnn.m
@@ -4,38 +4,42 @@ Hydro_Param = table2array(Hydro_Param);
 Turbin_Param = Hydro_Param(:, 1:4);
 
 %Data Division
-B_tip = Hydro_Param(:,1);
-B_70 = Hydro_Param(:,2);
-A_tip = Hydro_Param(:,3);
-A_70 = Hydro_Param(:,4);
-Cp = Hydro_Param(:, 5);
+ref_B_tip = Hydro_Param(:,1);
+ref_B_70 = Hydro_Param(:,2);
+ref_A_tip = Hydro_Param(:,3);
+ref_A_70 = Hydro_Param(:,4);
+ref_Cp = Hydro_Param(:, 5);
 
 %Transpose Data
 Turbin_Param = transpose(Turbin_Param);
-Cp = transpose(Cp);
+ref_Cp = transpose(ref_Cp);
 
 %rbnn param
 eg = 0.0001;
 sc = 100;
 
 %build model(rbnn)
-net = newrb(Turbin_Param, Cp);
+model_rbnn = newrb(Turbin_Param, ref_Cp);
 
-%random parameter
-Rand_Turbin_Param = readtable('RandParam.csv');
-Rand_Turbin_Param = table2array(Rand_Turbin_Param);
-Rand_Turbin_Param = transpose(Rand_Turbin_Param);
+
+%lhs random parameter
+lhs_B_tip = lhsdesign_modified(10000, 0.901,1.306);
+lhs_B_70 = lhsdesign_modified(10000, 00.913,1.198);
+lhs_A_tip = lhsdesign_modified(10000, 0.497,1.553);
+lhs_A_70 = lhsdesign_modified(10000, 0.489,1.466);
+lhs_turbin_param = horzcat(lhs_B_tip, lhs_B_70, lhs_A_tip, lhs_A_70);
+lhs_turbin_param = transpose(lhs_turbin_param);
 
 %run model(rbnn) with random parameter
-rand_Cp = net(Rand_Turbin_Param);
+rand_Cp = model_rbnn(lhs_turbin_param);
 
 %find max Cp & Turbin Param
 [max_Cp, Index] = max(rand_Cp);
 max_Cp;
 Index;
 
-Rand_Turbin_Param(:,Index);
+lhs_turbin_param(:,Index)
 
-%Paper Opt Paramter
+%Opt Paramter by reference(paper)
 Turbin_Param_Opt = [1.306;1.129;1.109;0.489];
-Cp_Opt = net(Turbin_Param_Opt);
+Cp_Opt = model_rbnn(Turbin_Param_Opt);
